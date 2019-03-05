c1cda22bfd644126c7f6759ac406a4b4cef923c4
Joe Tustin <j.tustin@gmail.com>
2019-03-05 23:39:15 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -7,12 +7,8 @@ def valid_move?(array, index)
   end
 end
 
+# I didnt need to but think this is how I could update the tic tac toe board
     # array[index] =
-
-
-
-
-
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def position_taken?(array, index)
